191f60d19e22336c72f48201d4d63e7421b8dfe0
Hodol <hkim9726@gmail.com>
2021-09-12 03:41:29 -0400

    first commit

--- etl.py
+++ etl.py
@@ -7,12 +7,26 @@ def handler(event, context):
     try:
         s3 = boto3.client('s3')
         dynamodb = boto3.client('dynamodb', region_name=region)
+        client = boto3.resource('dynamodb')
+        table = client.Table('covid-data2')
         confile = s3.get_object(Bucket='testbucket01234567899999', Key='nytdata_s3.csv')
         recList = confile['Body'].read().decode('utf-8').split('\n')
         firstrecord=True
         csv_reader = csv.reader(recList, delimiter=',', quotechar='"')
         lastrow = list(csv_reader)[-1]
-        response = dynamodb.put_item(
+        
+        
+        response = table.get_item(
+            Key={
+            'date': lastrow[0]
+            }
+        )
+        if 'Item' in response:
+            print('message 11111')
+            return
+        else:
+            print('message 2222222')
+            response2 = dynamodb.put_item(
             TableName='covid-data2',
             Item={
                 'date' : {'S':lastrow[0]},
